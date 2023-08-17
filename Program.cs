e4b3f2a12983b1311e29f763809fd18b31076b69
Elakilic <berfinela877@gmail.com>
2023-08-18 00:50:24 +0300

    Hangi işlemi hangi sembol ile yapacağımızın bilgisi verildi.

--- Program.cs
+++ Program.cs
@@ -6,3 +6,10 @@ int sayi1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("2.değeri girin : ");
 int sayi2 = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Toplama İşlemi İçin: +");
+Console.WriteLine("Çıkarma İşlemi İçin: -");
+Console.WriteLine("Çarpma İşlemi İçin: *");
+Console.WriteLine("Bölme İşlemi İçin: /");
+
+Console.WriteLine("İşlem seçin: + , - , * , /");
+string? islem =Console.ReadLine();

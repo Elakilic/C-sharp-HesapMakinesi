4477d8fa76a9a00e581681af1f921b8b2da7b8d4
Elakilic <berfinela877@gmail.com>
2023-08-18 00:44:35 +0300

    İşlem yapacağımız sayıları girmek için kod yazıldı.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Basit Hesap Makinesi
+
+Console.Write("1.Sayı : ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2.değeri girin : ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
